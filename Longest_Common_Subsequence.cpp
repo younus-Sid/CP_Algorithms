8438a1654fd089e84614acbe4e3a6d3c4eabfe24
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2022-06-14 13:56:28 +0530

    LCS_Program

---
+++ Longest_Common_Subsequence.cpp
@@ -0,0 +1,23 @@
+/*
+What is LCS
+Longest Common Subsequence is an algorithm that helps us find a subsequence which is common in two different sequences and is longest among all the common subsequences.
+It can be understood more clearly with the following example-
+sequence1 = ADCB
+sequence2 = DAC
+
+subsequences of sequence1 : {A, D, C, B, AD, AC, AB, DC, DB, CB, ADC, ADB, ACB, DCB, ADCB}
+subsequences of sequence2 : {D, A, C, DA, DC, AC, DAC}
+
+common subsequences are : {A, D, C, AC, DC}
+
+longest common subsequence : {AC, DC}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World!";
+  return 0;
+}
