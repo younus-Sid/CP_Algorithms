7fecfea3e6294937596c5b59cbd0de0463467fa7
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2022-07-02 22:32:41 +0530

    0/1 Knapsack_Algo

---
+++ Dynamic Programming/0-1 Knapsack Algorithm.py
@@ -0,0 +1,60 @@
+
+# This is the memoization approach of
+# 0 / 1 Knapsack in Python in simple
+# we can say recursion + memoization = DP
+ 
+# driver code
+
+val = [60, 100, 120 ]
+
+wt = [10, 20, 30 ]
+
+W = 50
+
+n = len(val)
+ 
+# We initialize the matrix with -1 at first.
+
+t = [[-1 for i in range(W + 1)] for j in range(n + 1)]
+ 
+ 
+
+def knapsack(wt, val, W, n):
+ 
+
+    # base conditions
+
+    if n == 0 or W == 0:
+
+        return 0
+
+    if t[n][W] != -1:
+
+        return t[n][W]
+ 
+
+    # choice diagram code
+
+    if wt[n-1] <= W:
+
+        t[n][W] = max(
+
+            val[n-1] + knapsack(
+
+                wt, val, W-wt[n-1], n-1),
+
+            knapsack(wt, val, W, n-1))
+
+        return t[n][W]
+
+    elif wt[n-1] > W:
+
+        t[n][W] = knapsack(wt, val, W, n-1)
+
+        return t[n][W]
+ 
+ 
+
+print(knapsack(wt, val, W, n))
+ 
+# This code is contributed by Prosun Kumar Sarkar
