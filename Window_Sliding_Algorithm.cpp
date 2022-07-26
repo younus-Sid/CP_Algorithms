c66a40a0d7d6d92dce8394453d242e6d5adc7d78
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2022-07-26 17:43:13 +0530

    Window Sliding Algo

--- Window_Sliding_Algorithm.cpp
+++ Window_Sliding_Algorithm.cpp
@@ -1,1 +1,7 @@
+/****************************************************************************************************
 
+The Sliding window is a problem-solving technique for problems that involve arrays/lists.
+These problems are easy to solve using a brute force approach in O(n^2) or O(n^3).
+Using the 'sliding window' technique, we can reduce the time complexity to O(n).
+
+****************************************************************************************************/
