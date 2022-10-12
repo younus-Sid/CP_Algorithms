e53bc3a8e4961eb732279f57152b393670be623c
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2022-10-12 14:48:41 +0530

    Morris Traversal Code

--- 005_Morris_Traversal.cpp
+++ 005_Morris_Traversal.cpp
@@ -1,57 +1,63 @@
 #include<iostream>
 using namespace std;
 
-class Node{
+class Node
+{
     public:
     int data;
     Node* left;
     Node* right;
 
-    Node(int d){
+    Node(int d)
+    {
         this->data=d;
         this->left=NULL;
         this->right=NULL;
     }
-
 };
-void morrisTraversal(Node *root){
+
+void morrisTraversal(Node *root)
+{
     Node* curr=root;
     Node* pre;
     if(root==NULL) return;
 
-    while(curr!=NULL){
-        if(curr->left==NULL){
+    while(curr!=NULL)
+    {
+        if(curr->left==NULL)
+        {
             cout<<curr->data<<" ";
             curr=curr->right;
         }
-        else{
+        else
+        {
             //Find inorder predecur
             pre=curr->left;
             while(pre->right!=NULL and pre->right!=curr)
                 pre=pre->right;
 
-            if(pre->right==NULL){
+            if(pre->right==NULL)
+            {
                 pre->right=curr;
                 curr=curr->left;
-                
             }
-            else{
+            else
+            {
                 pre->right=NULL;
                 cout<<curr->data<<" ";
                 curr=curr->right;
             }
-
         }
-
-
     }
-
 }
-Node* bulidTree(Node* root){
+
+Node* bulidTree(Node* root)
+{
     int d;
     cout<<"Enter data: ";
     cin>>d;
-    if(d==-1){
+    if(d==-1)
+    {
         return NULL;
     }
     root=new Node(d);
@@ -60,15 +66,16 @@ Node* bulidTree(Node* root){
     cout<<"Enter right child of "<<d<<": ";
     root->right=bulidTree(root->right);
     return root;
-
 }
-int main(){
+
+int main()
+{
     Node* root= NULL;
     root=bulidTree(root);
     morrisTraversal(root);
-    
 }
-/*
+
+/***********************************************************************************
 Enter data: 1
 Enter left child of 1: Enter data: 2
 Enter left child of 2: Enter data: 4
@@ -84,4 +91,5 @@ Enter left child of 3: Enter data: -1
 Enter right child of 3: Enter data: 6
 Enter left child of 6: Enter data: -1
 Enter right child of 6: Enter data: -1
-4 7 2 5 1 3 6 */
+4 7 2 5 1 3 6 
+*************************************************************************************/
